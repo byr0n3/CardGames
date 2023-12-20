b6e402f3ae3c659caf38bceaf5eab072084f3779
Byron <byronoud10@gmail.com>
2023-12-20 09:36:25 +0100

    base wild card logic

--- CardGames.Core/Uno/UnoGame.cs
+++ CardGames.Core/Uno/UnoGame.cs
@@ -99,7 +99,11 @@ namespace CardGames.Core.Uno
 
 			this.HandleCardEffects(card.Value);
 
-			this.NextTurn(UnoGame.GetNextPlayerModifier(this.flags));
+			// Don't go to the next turn if the player has to pick a color
+			if ((this.flags & UnoGameFlags.PickColor) == UnoGameFlags.None)
+			{
+				this.NextTurn(UnoGame.GetNextPlayerModifier(this.flags));
+			}
 
 			this.flags &= ~UnoGameFlags.SkipNext;
 
@@ -125,12 +129,30 @@ namespace CardGames.Core.Uno
 			this.OnGameStateChanged?.Invoke();
 		}
 
+		public void PickColor(UnoPlayer player, CardColor color)
+		{
+			if (!this.CanPickColor(player))
+			{
+				return;
+			}
+
+			this.TopCard = new Card(color, this.TopCard.Value);
+
+			this.flags &= ~UnoGameFlags.PickColor;
+
+			this.NextTurn(UnoGame.GetNextPlayerModifier(this.flags));
+
+			this.OnGameStateChanged?.Invoke();
+		}
+
 		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		public bool CanPlayCard(UnoPlayer player, Card card) =>
 			// Game is in progress
 			(this.State == GameState.InProgress) &&
 			// It's the player's turn
 			(this.Players.GetIndex(player) == this.CurrentPlayerIndex) &&
+			// Player doesn't have to pick a color (Wild color or +4)
+			((this.flags & UnoGameFlags.PickColor) == UnoGameFlags.None) &&
 			// Card can be played onto the current card
 			this.TopCard.CanPlay(card) &&
 			// Security check; player has the card they want to play
@@ -142,8 +164,19 @@ namespace CardGames.Core.Uno
 			(this.State == GameState.InProgress) &&
 			// It's the player's turn
 			(this.Players.GetIndex(player) == this.CurrentPlayerIndex) &&
+			// Player doesn't have to pick a color (Wild color or +4)
+			((this.flags & UnoGameFlags.PickColor) == UnoGameFlags.None) &&
 			// Player doesn't have a playable card
 			!player.HasPlayableCard(this.TopCard);
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public bool CanPickColor(UnoPlayer player) =>
+			// Game is in progress
+			(this.State == GameState.InProgress) &&
+			// It's the player's turn
+			(this.Players.GetIndex(player) == this.CurrentPlayerIndex) &&
+			// Game is waiting for player to pick a color
+			((this.flags & UnoGameFlags.PickColor) != UnoGameFlags.None);
 
 		private void HandleCardEffects(CardValue card)
 		{
@@ -158,9 +191,21 @@ namespace CardGames.Core.Uno
 					this.flags |= UnoGameFlags.SkipNext;
 					break;
 
-				case CardValue.DrawTwo or CardValue.DrawFour:
-					this.flags |= (card == CardValue.DrawTwo) ? UnoGameFlags.DrawTwo : UnoGameFlags.DrawFour;
+				case CardValue.Wild:
+					this.flags |= UnoGameFlags.PickColor;
 					break;
+
+				case CardValue.DrawTwo or CardValue.DrawFour:
+				{
+					this.flags |= (card == CardValue.DrawTwo) ? UnoGameFlags.DrawTwo : UnoGameFlags.DrawFour;
+
+					if (card == CardValue.DrawFour)
+					{
+						this.flags |= UnoGameFlags.PickColor;
+					}
+
+					break;
+				}
 			}
 		}
 
